c76d8ebfbff22a3b7f3d333615ebaacd64a0c111
Shubham <shubhamrajchauhan07@gmail.com>
2026-01-31 22:55:31 +0530

    leetcode 1975

---
+++ 1975.java
@@ -0,0 +1,30 @@
+/* 
+Leetcode : 1975
+https://leetcode.com/problems/maximum-matrix-sum/description/?envType=daily-question&envId=2026-01-05
+
+*/
+
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        int n = matrix.length;
+        long totalSum = 0;
+        int negCount = 0;
+        int minAbsolute = Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                totalSum += Math.abs(matrix[i][j]);
+                minAbsolute = Math.min(minAbsolute, Math.abs(matrix[i][j]));
+
+                if(matrix[i][j] < 0) negCount++;
+                
+            }
+        }
+
+        if(negCount % 2 == 0) { // even negatives
+            return totalSum;
+        }
+
+        return totalSum - 2 * minAbsolute; //odd negatives
+    }
+}
