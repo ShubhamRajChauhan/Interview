68ad6a66dccdaebe5d53ec85ee104d5e472d5e5e
Shubham <shubhamrajchauhan07@gmail.com>
2026-02-05 22:19:06 +0530

    leetcode 3379

---
+++ 3379.java
@@ -0,0 +1,19 @@
+/* 
+Leetcode : 3379
+https://leetcode.com/problems/transformed-array/description/?envType=daily-question&envId=2026-02-05
+*/
+
+
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int i=0; i<n; i++) {
+            int shift = nums[i] % n;
+            int idx = (i + shift) % n;
+            if(idx < 0) idx += n;
+            result[i] = nums[idx];
+        }
+        return result;
+    }
+}
